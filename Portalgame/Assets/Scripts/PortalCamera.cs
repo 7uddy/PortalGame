ec7eccc354f2274da5711759221892a63d976053
Lefter Andrei <ionut.lefter@student.unitbv.ro>
2024-12-23 20:06:19 +0200

    Changed portal prefab and worked on portal camera
    Changed portal prefab so camera won't be turned to 90 degrees while the other components are at 0
    Removed portal b prefab variant
    Changed portal camera so it partially works

--- Portalgame/Assets/Scripts/PortalCamera.cs
+++ Portalgame/Assets/Scripts/PortalCamera.cs
@@ -9,8 +9,13 @@ public class PortalCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
-        transform.position = portal.position + playerOffsetFromPortal;
-        //transform.eulerAngles.y += otherPortal.eulerAngles.y;
+        //Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        //transform.position = portal.position + playerOffsetFromPortal;
+    
+        float angularDifferenceBetweenPortals = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDiff = Quaternion.AngleAxis(angularDifferenceBetweenPortals, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDiff * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up); 
     }
 }
